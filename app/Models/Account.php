14b916a87fb582f1be972eb8ab3eb04cd51db877
EVOKE <evoke.lektrique@gmail.com>
2023-05-30 00:16:26 +0330

    Remove unused classes

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use App\Models\Order;
-use App\Models\Customer;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Account extends Model {
     use HasFactory;
@@ -27,7 +24,7 @@ class Account extends Model {
 
     protected $casts = [
         "expire_at" => "datetime",
-        "date" => "datetime",
+        "date"      => "datetime",
     ];
 
     public function order(): BelongsTo {
