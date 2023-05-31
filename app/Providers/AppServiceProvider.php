4bf498a9df0045a9c4f0331524db9ea259199a74
EVOKE <evoke.lektrique@gmail.com>
2023-05-31 18:16:06 +0330

    New stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -39,5 +40,10 @@ class AppServiceProvider extends ServiceProvider {
                     env('ADMIN_USER_EMAIL'),
                 ]);
         });
+
+        // Force HTTPS
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
